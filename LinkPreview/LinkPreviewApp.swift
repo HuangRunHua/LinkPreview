5d4be30c694415a264f9afd6d4bf0022d06ed422
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-04-19 14:15:18 +0800

    Initial Commit

---
+++ LinkPreview/LinkPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkPreviewApp.swift
+//  LinkPreview
+//
+//  Created by Huang Runhua on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
