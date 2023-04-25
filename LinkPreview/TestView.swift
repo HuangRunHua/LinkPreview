d79cd672f50dc5fbf26d0187aaba11d6cec72c4a
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-04-25 19:22:01 +0800

    Add code

--- LinkPreview/TestView.swift
+++ LinkPreview/TestView.swift
@@ -12,7 +12,7 @@ struct TestView: View {
     @Environment(\.openURL) var openURL
     
     let linkDataFetchers = [
-        LinkDataFetcher(link: "https://youtu.be/ANn9ibNo9SQ"),
+        LinkDataFetcher(link: "https://www.youtube.com/watch?v=ANn9ibNo9SQ"),
         LinkDataFetcher(link: "https://www.economist.com/special-report/2023/04/14/all-change"),
         LinkDataFetcher(link: "https://www.newscientist.com/article/mg25834340-300-how-i-found-a-new-home-online-at-mastodon-after-giving-up-on-twitter/?utm_term=Autofeed&utm_campaign=echobox&utm_medium=social&utm_source=Twitter#Echobox=1681858978"),
         LinkDataFetcher(link: "https://github.com/HuangRunHua/LinkPreview"),
